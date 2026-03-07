79aa93df31843980bcd680abcd882340d4ef9bd7
kik87827 <82139647+kik87827@users.noreply.github.com>
2026-03-07 22:56:11 +0900

    환경 세팅 스터디

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+export default defineConfig([
+	...nextVitals,
+	prettierRecommended,
+	{
+		rules: {
+			'no-undef': 'error',
+			// 프리티어 설정
+			'prettier/prettier': [
+				'error',
+				// 아래 규칙들은 개인 선호에 따라 prettier 문법 적용
+				// https://prettier.io/docs/en/options.html
+				{
+					singleQuote: true,
+					semi: true,
+					useTabs: true,
+					tabWidth: 2,
+					trailingComma: 'all',
+					printWidth: 80,
+					bracketSpacing: true,
+					arrowParens: 'avoid',
+				},
+			],
+		},
+	},
+
+	globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
-
-export default eslintConfig;
