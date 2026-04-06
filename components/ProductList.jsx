27dd9c3b27a3fb9e3538f9599d0f062ff8916c46
in kyung <kik87827@gmail.com>
2026-04-06 14:04:25 +0900

    진도중

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
+import styles from './ProductList.module.css';
 
 export default function ProductList() {
   const [products, setProducts] = useState(null);
@@ -9,5 +11,21 @@ export default function ProductList() {
       setProducts(response.data);
     });
   }, []);
-  return <ul>{products && products.map(({ id, name }, index) => <li key={id}>{name}</li>)}</ul>;
+  return (
+    <ul>
+      {products &&
+        products.map(({ id, name, price, imageUrl }, index) => {
+          return (
+            <>
+              <li key={id} className={styles.item}>
+                <div>
+                  <Image src={imageUrl} width={250} height={250} alt="" />
+                </div>
+                <div>{name}</div>
+              </li>
+            </>
+          );
+        })}
+    </ul>
+  );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        port: '',
+        pathname: '/user/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
